710356bc6f831b574b16cc3e627123b6b645eede
Eshaan Minocha <eshaanminocha0112@gmail.com>
2025-08-08 17:14:58 -0400

    Do tree questions

--- trees/deletion_of_nodes.py
+++ trees/deletion_of_nodes.py
@@ -62,3 +62,71 @@ def delNodes(root, to_delete):
         _ = dfs(root) # start dfs
         return list(forest_roots)
 
+# Q1
+"""
+Description - https://neetcode.io/problems/delete-node-in-a-bst?list=neetcode250
+Title - Delete Node in a BST
+Level - Medium
+---------------------------------------
+Question - 
+-> Delete a node with a given value in a BST
+
+Thoughts - 
+-> Read comments below
+-> Time: O(h)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root, key):
+        if not root: return root # base case
+        
+        # Search for key
+        if key < root.val :
+            root.left = self.deleteNode(root.left, key) # assign node = dfs(node) to return updated tree
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else: # key found
+            # If at this position there is no left node of the key, we can simply return the right subtree of the node
+            if not root.left: return root.right
+
+            # Vice-Versa
+            if not root.right: return root.left
+
+            '''
+            If both left and right subtrees of the node to be deleted exist, we need to assign the minimum of right subtree as the "propagate up" value to maintain the BST properties
+            Check this example: key=2
+                    8                               8
+                2       9       --soln-->       3       9   
+            1       4                         1   4   
+                  3   5                             5 
+
+            Here when we're deleting node 2, the "propagate up" value is node 3 (which is the minimum of the right subtree of 2)
+            Since we're finding the minimum value of the right subtree, it is guaranteed that the left subtree of the key node will be
+            less than the new "propagate up" value (1<min(right subtree of 2) = 1<3) and the rest of the values will be greater (4 and 5 > 3)
+            '''
+            # Find minimum of the right subtree and replace its value for the key node to be deleted
+            cur = root.right
+            
+            while cur.left:
+                cur = cur.left
+            minimum_of_right_subtree = cur.val
+            root.val = minimum_of_right_subtree
+
+            # At this point, the tree will look like this:
+            '''
+                                8
+                            3       9
+                        1      4
+                              3   5
+            We can see that even though we've replaced the node to be deleted, we are left to delete it from the position it was taken from, so we
+            need to recursively delete that one as well
+            '''
+            root.right = self.deleteNode(root.right,minimum_of_right_subtree) 
+
+        return root
--- trees/general_dfs.py
+++ trees/general_dfs.py
@@ -72,3 +72,115 @@ def flipEquiv(root1,root2) -> bool:
 
         
         return dfs(root1,root2) # simple trigger of dfs
+
+
+# Q2
+"""
+Description - https://neetcode.io/problems/insert-into-a-binary-search-tree?list=neetcode250
+Title - Insert in a BST
+Level - Medium
+---------------------------------------
+Question - 
+-> Insert a given node in a BST
+-> Eg:          5
+            3       9
+        1       
+        val=6 (to be inserted)
+    Soln: Insert to the right of 5 but left of 9
+
+Thoughts - 
+-> Brute Force:
+    -> Go through each node of the tree, add it to an array, sort the array, add the `val` to the sorted array in the sorted position, rebuild the tree
+    -> Time: O(n log n); Space O(n)
+-> Optimal:
+    -> Make use of the sorting property in a BST (smaller values to the left of a node, larger values to the right of the node)
+    -> Take the above eg, we check if 6 < or > the root, 5 and DFS to the right (since 6>5), then we DFS in a similar manner till we reach the left of 9 and add the value 6 and return
+    -> The thing to note here is the return value of the DFS. Say we are at dfs(9) function call:
+        -> We check 6 < 9 so we now need to dfs(9.left) i.e dfs(none)
+        -> If we reach a none node that means we need to add a node with `val`, so we build a node(val=6) here
+        -> We must return the node that's been created and assign it to the correct position (i.e the position it was dfs-ed from which was to the left of 9)
+        -> So finally, something like:
+            node.left = dfs(node.left) where the return value of dfs is the value that was added
+        -> ** For any other node, this way where we do node = dfs(node), it propagates up the tree/graph with the value at the dfs position
+    -> Time: O(h), h=height of tree since at each height level, we have 1 comparison
+""" 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(root,val):
+
+        def dfs(node):
+            """
+            If val<node -> go left
+            If val>node -> go right
+            
+            The main idea to keep in mind here is that we propagate back up the same node that's traversed, so the links
+            between the tree nodes are maintained.
+            """
+
+            nonlocal val # to make sure the nested function reads the outer function's val
+
+            if not node: return TreeNode(val)
+
+            # Search
+            if val < node.val:
+                node.left = dfs(node.left)
+            elif val > node.val:     
+                node.right = dfs(node.right) 
+            return node          
+        
+        root = dfs(root) # trigger
+        return root
+
+
+# Q2
+"""
+Description - https://neetcode.io/problems/binary-tree-right-side-view?list=neetcode250
+Title - Binary tree right side view
+Level - Medium
+---------------------------------------
+Question - 
+-> Return elements that can be viewed from the right
+-> Eg1:                         1
+                        2               3
+                    4       5         6
+        soln: [1,3,6]
+-> Eg2:
+                                1
+                        2
+                    5       3
+        soln: [1,2,3]
+
+Thoughts -
+-> We want to view the tree only from the right side. Ideally we want to return elements that are the right-most
+-> In a DFS approach, we can think of simply just traversing to the right of each node, but what if we get an example like Eg2 above.
+    -> There are no nodes to the right of 1, so we can't JUST traverse right
+    -> We must traverse right first (if there is an element there, we add it)
+    -> Then we traverse left - if there is not element to the right, then we add element to the left
+-> How do we know which element to add? We must add the very first element during our DFS we see.
+    -> This way, we'll guarantee to add the right-most element as we traverse the right subtree first
+-> Time: O(h) --> add 1 element per depth    
+"""
+def rightSideView(root):
+    res = []
+    
+    def dfs(node, level):
+        """ DFS with priority to the right and add first(rightmost) element per level """
+        if not node: return None 
+
+        # If this node is the first at the current level, add it
+        if level == len(res): # we'll be able to see only one element per level from the right
+            res.append(node.val)
+        
+        # Try to find element to the right first
+        dfs(node.right, level+1)
+        dfs(node.left, level+1)
+
+    dfs(root,level=0) # need to pass in level to idenitfy which is the first element at that level we are seeing
+
+    return res
+
