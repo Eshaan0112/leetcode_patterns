b5780288b5c0528e278e8c9b2f32819f42255be1
Eshaan Minocha <eshaanminocha0112@gmail.com>
2025-08-23 01:53:41 -0400

    Add trick of avoiding duplicates in backtracking

---
+++ backtracking.py
@@ -0,0 +1,42 @@
+''' Backtracking questions from Neetcode
+
+VANILLA BACKTRACKING:
+-> Think in terms of branching a tree (it's possible it can have > 2 branches) or just a binary (to include or not to include) choice.
+-> Questions where we need to list all possible solutions of a given problem is the main use case for this approach
+-> It's a brute force approach and in terms of code, follows a template like:
+     
+    def f():
+    
+        # operations 
+
+        # Recursively backtrack
+        def backtrack(i):
+            # Base case 
+            if some_condition:
+                # reached leaf in our backtracking tree as all subsets will be in leaves
+                # remember if you're dealing with an array of sorts, you might want to do a copy of it here since its reference may be modified in future steps
+                return something
+
+            # Choose to do something (maybe .append to an array)
+            backtrack(i+1) # explore all paths where we chose to do something
+
+            # choose not to do something (maybe .pop after exploring what happens after appending)
+            backtrack(i+1) # explore all paths where we chose not to do something
+        backtrack(0) # start from 0th index
+        return something
+-> Time Complexity: O(2^n) --> 2 choices for n elements
+
+AVOIDING DUPLICATES:
+-> Think sorting
+-> In terms of application, say we are to avoid duplicates in a binary choice approach. Then we notice that the duplicates occur in between
+paths that are chosen to be included and not to be included. For eg, let's say input = [1,2,1] and we're making subsets
+                                    *
+                        1                     []
+                    12      1               2       []
+                121  12   11  1           21
+    In the leaves, choice to include 1 at the first level leads to [12] in the leaf and then choice not to include 1 leads to [21]. [12] and [21] are duplicates.
+    We need to make it so that once we've traversed the choice-to-include using duplicate elements, we make sure we skip the duplicates before we start our traversal
+    down the path of choice-not-to-include. In the code, this should be done after popping back up (check subsets 2 problem on Neetcode.io)
+                            
+'''
+
