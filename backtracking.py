1f4e0a07cede513cb6b517ec5529d191c0ed36ce
Eshaan Minocha <eshaanminocha0112@gmail.com>
2025-08-24 15:44:41 -0400

    Do sliding window max

--- backtracking.py
+++ backtracking.py
@@ -60,7 +60,173 @@ def f():
             
             backtrack(i+1) # explore all paths where we chose not to do something
         backtrack(0) # start from 0th index
-        return something
-                            
+        return something                    
 '''
 
+
+''' Vertical Path Solutions (i.e in a tree, each path is a solution)
+->Code:
+    def main(input):
+        # intialize things to be returned
+        def dfs(i):
+            # Base cases
+                
+            # Explore each vertical path
+            for j in range(i, len(input)):
+                parts.append(input[i:j+1])
+                dfs(i=j+1)
+                parts.pop()
+        dfs(0)
+        return whatever
+        
+    -> How the vertical paths are being built (Consider Q2 below, Palindrom Partitioning - the visual explanation is shown only till "ab"):
+                                                    "a"                 "aa"             "aab"     
+                                                "a"     "ab" (here!!)
+                                                "b"
+                
+            
+            -> dfs(i=0) => j = 0                                 ,                                j=1               ,..                                    j=2  
+                               |
+                               |
+                check s[i:j+1],s[0:1]->"a"(Palindrome)
+                               |
+                               |
+                               dfs(i=1) => j = 1 ,..                                           j=2 (here i=1, j=2, so we check s[i:j+1] which is "ab") 
+                                               |                                                  |
+                                               |                                                  |
+                                    check s[i:j+1],s[1:2]->"a"(Palindrome)                        |
+                                               |                                                  |
+                                               |                                       pop(a), j is moved forward as part of the for loop
+                                               dfs(i=2) => j = 2                           nothing to explore as j == len(s)  
+                                               |                                                  |
+                                               |                                       pop(b), j is moved forward as part of the for loop
+                                    check s[i:j+1], s[2:3]->"b"(Palindrome)                       |
+                                               |                                                  |
+                                               |                                                  |
+                                               dfs(i=3) => out of bounds -> res = [["a","a","b"]] |
+                                               
+                                          
+                               
+
+'''         
+# Q1
+"""
+Description - https://neetcode.io/problems/permutations?list=neetcode250
+Title - Permutations
+Level - Medium
+---------------------------------------
+Question - 
+-> Given unique ints, list all possible permutations
+-> Eg:
+    input = [1,2,3]
+    soln = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Thoughts - 
+-> "all possible" --> brute force --> backtracking
+
+-> Difficult brute force:
+                                                                                     *
+all 3 available for us to choose                                    1                2              3
+2 out of 3 available for us to choose                           2       3        1       3      1       2
+1 out of 3 available for us to choose                           3       2        3       1      2       1    
+    -> See the vertical paths (all paths)- they are the permutations.
+    -> This way is possible and gives an intuition about why permutations are n!
+
+-> Easy brute force:
+    -> Let's not think in terms of branching
+    -> Say we have [1,2,3] 
+        -> Building permuatations of [1,2,3] involves moving "1" through all positions of all permutations of [2,3]
+                -> Moving 1 through all positions of [2,3] and [3,2] gives us [1,2,3],[2,1,3],[2,3,1] and [1,3,2],[3,1,2],[3,2,1] respectively
+            -> Building permutations of [2,3] involves moving "2" through all permuations of [3]
+                -> Moving 2 through all positions of [3] gives us [2,3] and [3,2]
+        -> This gives a recursive subproblem in that the subproblem is getting permutations (by moving through possible positionsm) of the sublist by excluding the element which we want to keep locked
+        -> [1,2,3]   ---> return with [1,2,3],[2,1,3],[2,3,1] and [1,3,2],[3,1,2],[3,2,1]
+            [2,3]  ----> pop back up with [2,3] and [3,2] 
+            [3] ----> pop back up with [3] since [3] is the only permuation of [3]
+        -> We just need to realize all permutations: [a or b or c, a or b or c, a or b or c] can be built by having permuations of [b,c] and so on
+            -> Building the permutations needs the realization that we need to place the "excluded" element (a) at every possible position of every permutation of [b,c]
+            
+        -> Time: O(n!)
+        """ 
+
+def permute(self, nums):
+        res = [] # contains all permutations, n! of them where n=len(nums)
+
+        # Recursive call to get permutations of sublist of nums i.e nums minus the first element
+        '''
+        If we're recursing for nums = [1,2,3], the recursive stack would be something like
+        permute([1,2,3]) --> permute([2,3]) --> permute([3]) --> permute([])
+           nums[0] = 1        nums[0] = 2        nums[0] = 3        
+                            return [[2,3],[3,2]] return [[3]]    return[[]]
+        '''
+
+        # Base case
+        if len(nums) == 0: return [[]] 
+        permutations = self.permute(nums[1:]) # nums[0] is excluded 
+
+        for p in permutations: # for all perutations for a sublist of nums
+            for i in range(len(p)+1): # for all possible positions where nums[0] can be inserted
+                '''
+                say we are at p=[2,3] for permutations= [[2,3],[3,2]], i here tells us what positions nums[0]=1 can
+                be positioned at. If we position 1 at every possible i, then we get all permutations for this p
+                like: i=0 ([1,2,3]); i=1 ([2,1,3]); i=2 ([2,3,1])
+                '''
+                snapshot_of_permutation = p.copy()
+                snapshot_of_permutation.insert(i, nums[0]) 
+                res.append(snapshot_of_permutation)
+        
+        return res
+
+# Q2
+"""
+Description - https://neetcode.io/problems/palindrome-partitioning?list=neetcode250
+Title - Palindrome Partitioning
+Level - Medium
+---------------------------------------
+Question -
+    -> Given a string s, split s into substrings where every substring is a palindrome
+    -> Eg: s = "aab"
+           Soln: [["a","a","b"],["aa","b"]]
+Thoughts - 
+    -> We will essentially need to check all possible partitions
+    -> Possible choices of partitions can be derived by cutting the input array at each position, so first choice would be:
+        "a"               "aa"              "aab"
+        -> For leftmost "a", we've cut as "a" | "ab"     
+        -> We can further split the right part "ab" by performing two cuts- "a" | "b" AND "ab"|, so the branch starting with "a" will look like:
+                        "a"                 "aa"             "aab"     
+                    "a"     "ab"
+                    "b"
+    
+        -> Look at the vertical paths - these are the possible partitions and we need to check each of these paths to see if they're palindromes
+                 
+                                     
+"""
+
+def partition(self,s):
+        
+        res = [] # store all palindromic partitions
+        parts = [] # partitioned substrings 
+
+        def dfs(i):
+            """ DFS backtrack starting at ith level of recursive tree """
+            
+            # Reached end of string, s
+            if i == len(s):
+                res.append(parts.copy())
+                return
+            
+            # Explore each vertical path
+            for j in range(i, len(s)):
+                if self.isPali(s,i,j): # check if substring s[i:j+1] is a palindrome
+                    parts.append(s[i:j+1])
+                    dfs(i=j+1)
+                    parts.pop()
+        dfs(0) # trigger
+        return res
+        
+def isPali(self, s, l, r):
+    while l < r:
+        if s[l] != s[r]:
+            return False
+        l, r = l + 1, r - 1
+    return True
--- sliding_window.py
+++ sliding_window.py
@@ -134,7 +134,102 @@ def longestrepeatingreplacement(s,k):
         
     return longest
 
+# Q4
+"""
+Description - https://neetcode.io/problems/sliding-window-maximum?list=neetcode150
+Level - Hard
+Title - Sliding window maximum
+---------------------------------------
+Question -  
+-> Read the problem
 
+-> Eg: 
+Input: nums = [1,2,1,0,4,2,6], k = 3
+
+Output: [2,2,4,4,6]
+
+Explanation: 
+Window position            Max
+---------------           -----
+[1  2  1] 0  4  2  6        2
+ 1 [2  1  0] 4  2  6        2
+ 1  2 [1  0  4] 2  6        4
+ 1  2  1 [0  4  2] 6        4
+ 1  2  1  0 [4  2  6]       6
+
+Thoughts - 
+    -> Brute Force:
+        -> Shift through each window as given and keep track of max as you go
+        -> Time: O(n*k) where n=number of elements, k=size of window --> For each window we need to find max (O(n*k))
+
+    -> Sub-optimal:
+        -> We can add each window to a max heap to get the max at that window
+        -> Time: O(n log n) --> Adding to the heap is log n and has to be done n times
+    
+    -> Optimal:
+        -> Use a monotonically decreasing double ended queue i.e DE-QUE
+            -> Why a deque?
+                -> O(1) to push from either end of the deque
+                -> O(1) to add to the right of the deque
+                -> deque = [left(front),.....,right(back)]
+                -> montonically decreasing deque = [max_element, 2nd max_element, 3rd max_element,....,smallest_element]
+                    -> Why monotonically decreasing:
+                        -> We need to keep the max of the current window we are traversing to the left and any future max's that become the max of the next window to the right once the current max expires from the window
+                            -> Eg: say our deque = [4,3,2,1] and k = 2
+                                then [4,3,2,1] 
+                                      --       -> current window, and max=4
+                                      [4,3,2,1]
+                                         ---   -> new window and now 4 is not in our new window
+            -> Process of using a deque:
+                -> Consider this eg: nums = [8,7,6,9], k=2
+                    -> Process 
+                    Step1: 8  7  6  9
+                           ----             -> deque = [8,7] since 7<8, our deque would be monotonically decreasing if we add 7 (7 can potentially the max of the next window)
+                                            -> res = [8] -> left most value in the deque will be the max of the current window, so we can add that to our result
+                    
+                    
+                    Step2: 8  7  6  9
+                              ----          -> 8 is no longer in our window, so we pop from the left (O(1)); deque = [7]
+                                            -> deque = [7,6] since 6<7, our deque would be monotonically decreasing if we add 6
+                                            -> res = [8,7] -> left most value in the deque will be the max of the current window, so we can add that to our result 
+   
+                    Step3: 8  7  6  9
+                                 ----       -> 7 is no longer in our window, so we pop from the left (O(1)); deque = [6]
+                                            -> Now we have 9 and we check if 9 > 6, that means we need to pop the right most position (NOT THE LEFT!)
+                                            -> We keep popping from the right till we get an element > 9. It just so happens that our deque=[6], so we end up popping just 6 and get deque = [9]
+                                            -> res = [8,7,9]: FINAL RESULT
+"""
+def maxSlidingWindow(nums, k):
+        '''
+        NOTE: We are storing indices in the de-que for this problem
+        '''
+        res = [] # max of each window - final output
+        l,r = 0,0 # pointers to slide window
+        
+        de_que = deque() # [left(front),...........,right(back)]
+
+        while r<len(nums): # as long as we don't reach end of input
+
+            # Remove values that are smaller in the current window as they can never be the max in the current window or any future windows
+            while de_que and nums[de_que[-1]] < nums[r]: # nums[de_que[-1]] is the rightmost value of the deque
+                de_que.pop() # FROM RIGHT --> O(1)
+            
+            # Add element to the queue --> the max of the current window
+            de_que.append(r) 
+
+            # Remove the element that expires once the window is shifted
+            if l > de_que[0]:
+                de_que.popleft() # FROM LEFT --> O(1)
+            
+            '''Edge case: we only need to update left pointer if our window is of the given size
+            At the very start, the window is still growing - it isn’t size k yet.
+            We only want to record results (and slide the left pointer l) once the window reaches size k'''
+            if (r-l+1) == k:
+                res.append(nums[de_que[0]]) # Add the leftmost value to the result as it contains the max of the current window
+                l += 1
+            r += 1 # keep growing the window
+        
+        return res
         
             
     
